6df6788808c09f95801aba97680838a7edd5ea6f
zhaojiedi <zhaojiedi1992@outlook.com>
2019-09-06 18:59:21 +0800

    delete kafka

--- aliyun_exporter/DescribeKafkaRequest.py
+++
@@ -1,6 +0,0 @@
-from aliyunsdkcore.client import AcsClient
-from aliyunsdkcore.request import CommonRequest
-class DescribeKafkaRequest(CommonRequest):
-    def __init__(self):
-
-        super(DescribeKafkaRequest,self).__init__()
